f823df23fd6ea2434a890155bb05fbf45107b646
cannedcake <78706045+cannedcake@users.noreply.github.com>
2021-02-07 15:03:44 -0700

    cake's bitcoin miner version one
    idk

---
+++ cake's Miner.bat
@@ -0,0 +1,61 @@
+@echo off
+color 5
+echo loading scripts.
+color 4
+echo loading scripts..
+color 2
+echo loading scripts...
+color 3
+echo Ready
+color 7
+echo warming up hashing algorithm
+color 5
+echo warming up hashing algorithm.
+color 8
+echo warming up hashing algorithm..
+color 1
+echo warming up hashing algorithm...
+color 3
+echo Done
+
+color 4
+echo connecting to bitcoin wallet.
+color 2
+echo connecting to bitcoin wallet..
+color 6
+echo connecting to bitcoin wallet...
+
+color 3
+echo Done
+
+
+color 4
+echo connecting to bitcoin network.
+color 7
+echo connecting to bitcoin network..
+color 5
+echo connecting to bitcoin network...
+color 3
+echo Done
+color 7
+echo Proccessing Blockchain.
+color 2
+echo Proccessing Blockchain..
+color 8
+echo Proccessing Blockchain...
+
+color 3
+echo Done
+
+color 3
+echo Ready to mine
+:loop
+echo Mining bitcoin
+color 4
+echo Mining bitcoin.
+color 5
+echo Mining bitcoin..
+color 6
+echo Mining bitcoin...
+color 9
+goto loop
